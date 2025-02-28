894237a2586e545dc26fac5406c7bf00c68ab715
BetoWH13 <alberto@bintbiz.com>
2025-02-28 15:47:54 -0600

    Fix API response format to match frontend expectations

--- api/bulk.js
+++ api/bulk.js
@@ -10,6 +10,6 @@ exports.handler = async function(event, context) {
     
     return {
         statusCode: 200,
-        body: JSON.stringify({ wifi_names: selectedNames })
+        body: JSON.stringify({ names: selectedNames })
     };
 };
--- api/random.js
+++ api/random.js
@@ -8,6 +8,6 @@ exports.handler = async function(event, context) {
     
     return {
         statusCode: 200,
-        body: JSON.stringify({ wifi_name: randomName })
+        body: JSON.stringify({ name: randomName })
     };
 };
